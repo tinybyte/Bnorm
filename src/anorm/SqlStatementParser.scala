9183627ff2a04f1730269eee0c65754025418547
TinyByte <evolye@gmail.com>
2012-06-06 00:08:31 +0800

    add comment

--- src/anorm/SqlStatementParser.scala
+++ src/anorm/SqlStatementParser.scala
@@ -9,14 +9,32 @@ object SqlStatementParser extends JavaTokenParsers {
     (r.flatMap(_._1).mkString, (r.flatMap(_._2)))
   }
 
+  /**
+   * inputStr ::= {literal | variable | other }
+   * @return
+   */
   def instr: Parser[List[(String, Option[String])]] = rep(literal | variable | other)
 
+  /**
+   * literal ::= stringLiteral | simpleQuotes
+   * @return
+   */
   def literal: Parser[(String, Option[String])] = (stringLiteral | simpleQuotes) ^^ { case s => (s, None) }
 
+  /**
+   * variable ::= "{" ident "." ident "}"
+   * the grammer for the parameter is actually i1.i2, but the . has nothing to do with what we want,
+   * so we'll keep only the right part using ~>
+   * @return
+   */
   def variable = "{" ~> (ident ~ (("." ~> ident)?)) <~ "}" ^^ {
     case i1 ~ i2 => ("?": String, Some(i1 + i2.map("." + _).getOrElse("")))
   }
 
+  /**
+   * other ::= "."
+   * @return
+   */
   def other: Parser[(String, Option[String])] = """.""".r ^^ {
     case element => (element, None)
   }
